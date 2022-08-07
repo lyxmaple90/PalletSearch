fb8d752d96ebb55f689373cb5ad7bbe6c6c53682
lyxmaple90 <79389466+lyxmaple90@users.noreply.github.com>
2022-08-07 15:13:45 +0800

    Update test.php

--- test.php
+++ test.php
@@ -6,18 +6,12 @@ $port = 25060;
 $database = "Pallet";
 
 
-  // mysqli 的四個參數分別為：伺服器名稱、帳號、密碼、資料庫名稱
+
   $conn = mysqli_connect($host, $username, $password,$database, $port);
 
 
   
-  if($conn){
-    mysqli_query($conn,'SET NAMES uff8');
-    // echo "正確連接資料庫";
-}
-else {
-    echo "不正確連接資料庫</br>" . mysqli_connect_error();
-}
+
 mysqli_close($conn);
   echo("OK");
 ?>
